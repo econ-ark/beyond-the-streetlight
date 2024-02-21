2396b69400a1e52d0615a07e51a0abef44652d82
alanlujan91 <alanlujan91@gmail.com>
2024-02-21 08:35:32 -0500

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -4,6 +4,9 @@ SCPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
 cd "$SCPT_DIR"
 
+mamba env update -f binder/environment.yml
+conda activate rs100_discussion
+
 ipython *.ipynb
 
 python code/main/reproduce.py
