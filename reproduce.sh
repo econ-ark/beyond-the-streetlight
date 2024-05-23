ad8102429aa87bbf5f6ad523d03fe0a6ce1b2744
Cameron Riddell <cameron@dutc.io>
2024-05-23 06:45:49 -0700

    remove mamba & automatic env activation

--- reproduce.sh
+++ reproduce.sh
@@ -4,9 +4,6 @@ SCPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
 cd "$SCPT_DIR"
 
-mamba env update -f binder/environment.yml
-source activate rs100_discussion
-
 ipython *.ipynb
 
 python code/main/reproduce.py
